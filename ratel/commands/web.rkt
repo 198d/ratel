ec99ee8841d9c26d9aa6c59d1d60a3292fc4146d
John MacKenzie <john@nineteeneightd.com>
2016-12-20 20:14:20 +0000

    commands/web.rkt: Configured mount timeouts and global --mount-timeout
    This adds a (thread) to the server that monitors mounted mounts and
    ensures they're unmounted after their configured timeout and adds an
    option for applying a global timeout for all mounts. The global timeout
    overrides a configured timeout. Currently, this is probably most useful
    with the --unshare option of `ratel` to ensure mounted mounts don't
    "leak" after shutdown of the server.

--- ratel/commands/web.rkt
+++ ratel/commands/web.rkt
@@ -2,6 +2,8 @@
 (require openssl
          web-server/web-server
          web-server/private/dispatch-server-sig
+         "../config.rkt"
+         "../suid-helper.rkt"
          "../web/dispatcher.rkt")
 
 
@@ -10,6 +12,7 @@
 
 (define listen-ip (make-parameter "127.0.0.1"))
 (define listen-port (make-parameter 8080))
+(define mount-timeout (make-parameter 0))
 (define ssl-server-cert (make-parameter #f))
 (define ssl-server-key (make-parameter #f))
 (define ssl-client-cert (make-parameter #f))
@@ -43,6 +46,9 @@
                 (listen-port port)]
     [("--ip-address") ip-address "IP address to listen on"
                       (listen-ip ip-address)]
+    [("--mount-timeout") timeout
+                         "Time, in seconds, that mounts should remain mounted"
+                         (mount-timeout (string->number timeout))]
     [("--ssl-server-cert") server-cert "Path to server certificate file"
                            (ssl-server-cert server-cert)]
     [("--ssl-server-key") server-key "Path to server private key file"
@@ -79,5 +85,34 @@
           (let loop ()
             (display (vector-ref (sync rcvr) 1) (current-error-port))
             (loop)))))
+
+    (thread
+      (lambda ()
+        (let loop ([mount-times (hash)])
+          (sleep 1)
+          (let ([current-time (current-seconds)])
+            (loop
+              (for/fold ([mount-times mount-times])
+                        ([mount-config (in-list (read-all-mount-configs))])
+                (let ([timeout (if (> (mount-timeout) 0)
+                                 (mount-timeout)
+                                 (get-in mount-config '(mount timeout)))])
+                  (if (> timeout 0)
+                    (let* ([mount-name (get-in mount-config '(name))]
+                           [mount-mounted? (mounted? mount-config)]
+                           [mount-time (hash-ref mount-times mount-name
+                                                 current-time)])
+                      (cond [(and mount-mounted?
+                                  (> (- current-time mount-time) timeout))
+                             (suid-umount mount-config)
+                             (hash-remove mount-times mount-name)]
+                            [(and mount-mounted?
+                                  (not (hash-has-key? mount-times mount-name)))
+                             (hash-set mount-times mount-name current-time)]
+                            [(and (not mount-mounted?)
+                                  (hash-has-key? mount-times mount-name))
+                             (hash-remove mount-times mount-name)]
+                            [else mount-times]))
+                    mount-times))))))))
 
     (do-not-return)))
